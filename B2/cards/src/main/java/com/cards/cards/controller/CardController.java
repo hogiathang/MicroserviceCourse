1ab6168824c0561f9d63ed590e658ec81430f498
RavenTheshadow <hogiathang2@gmail.com>
2025-01-04 07:23:34 +0700

    Card service

---
+++ B2/cards/src/main/java/com/cards/cards/controller/CardController.java
@@ -0,0 +1,75 @@
+package com.cards.cards.controller;
+
+import com.cards.cards.constants.CardsConstants;
+import com.cards.cards.dto.CardsDto;
+import com.cards.cards.dto.ResponseDto;
+import com.cards.cards.service.ICardsService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+@AllArgsConstructor
+@Validated
+public class CardController {
+
+    private ICardsService cardsService;
+
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createCard(
+            @RequestParam  @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+            String mobileNumber) {
+        cardsService.createCard(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(CardsConstants.STATUS_201, CardsConstants.MESSAGE_201));
+    }
+
+    @GetMapping("/fetch")
+    public ResponseEntity<CardsDto> fetchCard(
+            @RequestParam @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+            String mobileNumber) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(cardsService.fetchCard(mobileNumber));
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateCard(
+            @Valid @RequestBody
+            CardsDto cardsDto
+    ) {
+        boolean isUpdated = cardsService.updateCard(cardsDto);
+        if (!isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto(CardsConstants.STATUS_417, CardsConstants.MESSAGE_417_UPDATE));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(CardsConstants.STATUS_200, CardsConstants.MESSAGE_200));
+        }
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteCard(
+            @RequestParam @Pattern(regexp="(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
+            String mobileNumber
+    ) {
+        boolean isDeleted = cardsService.deleteCard(mobileNumber);
+        if (!isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto(CardsConstants.STATUS_417, CardsConstants.MESSAGE_417_DELETE));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(CardsConstants.STATUS_200, CardsConstants.MESSAGE_200));
+        }
+    }
+}

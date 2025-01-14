6615b7f02e243d81a336736a49dd4d78798599ef
RavenTheshadow <hogiathang2@gmail.com>
2025-01-15 02:35:22 +0700

    New B8

---
+++ B8/v3.2-springboot/accounts/src/main/java/com/accounts/accounts/mapper/CustomerMapper.java
@@ -0,0 +1,20 @@
+package com.accounts.accounts.mapper;
+
+import com.accounts.accounts.dto.CustomerDto;
+import com.accounts.accounts.entity.Customer;
+
+public class CustomerMapper {
+    public static CustomerDto mapToCustomerDto(Customer customer, CustomerDto customerDto) {
+        customerDto.setName(customer.getName());
+        customerDto.setEmail(customer.getEmail());
+        customerDto.setMobileNumber(customer.getMobileNumber());
+        return customerDto;
+    }
+
+    public static Customer mapToCustomer(CustomerDto customerDto, Customer customer) {
+        customer.setName(customerDto.getName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setMobileNumber(customerDto.getMobileNumber());
+        return customer;
+    }
+}
